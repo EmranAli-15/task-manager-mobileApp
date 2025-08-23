507fd480daadb7f3ddf1018c8bc31fab39fbadc0
EmranAli-15 <mohammademranali15@gmail.com>
2025-08-23 18:51:11 +0600

    working on skeleton

--- components/ui/NoteCardSkeleton.tsx
+++ components/ui/NoteCardSkeleton.tsx
@@ -1,10 +1,42 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React, { useEffect, useRef } from 'react';
+import { Animated, Easing, View } from 'react-native';
 
-export default function NoteCardSkeleton() {
-  return (
-    <View>
-      <Text style={{color: "gray"}}>NoteCardSkeleton</Text>
-    </View>
-  )
-}
+const SkeletonCard = () => {
+    const pulseAnim = useRef(new Animated.Value(0.5)).current;
+
+    useEffect(() => {
+        Animated.loop(
+            Animated.sequence([
+                Animated.timing(pulseAnim, {
+                    toValue: 0.8,
+                    duration: 1000,
+                    easing: Easing.linear,
+                    useNativeDriver: true,
+                }),
+                Animated.timing(pulseAnim, {
+                    toValue: 0.5,
+                    duration: 1000,
+                    easing: Easing.linear,
+                    useNativeDriver: true,
+                }),
+            ])
+        ).start();
+    }, [pulseAnim]);
+
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', marginTop: 100, alignItems: 'center' }}>
+            <Animated.View
+                style={{
+                    width: 100,
+                    height: 100,
+                    backgroundColor: 'gray',
+                    borderRadius: 50,
+                    opacity: pulseAnim
+                }}
+            />
+        </View>
+    );
+};
+
+
+export default SkeletonCard;
